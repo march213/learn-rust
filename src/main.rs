f4ac4c14bc028065904daca8d96297f4d4a9baa5
march213 <jane.molodetskaya@gmail.com>
2022-07-08 00:03:40 -0400

    added a 2d camera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
